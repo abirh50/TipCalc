34755993513746744b9b402fbcfeab5bd719256d
Abir Haque <abirhaque@outlook.com>
2020-09-13 20:05:29 -0400

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Abir Haque on 9/13/20.
+//  Copyright © 2020 Abir Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
